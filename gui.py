6b0f6e6db67ab16d12694732cc586a1d376ca091
Marko Milenkovic <mmarkomile@gmail.com>
2024-08-08 03:45:15 +0200

    more improvements to gui

--- gui.py
+++ gui.py
@@ -7,6 +7,68 @@ import squarify
 import mplcursors
 import numpy as np
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+# program
+from excel_parser import *
+from webscraper import *
+from data_anal import *
+
+tickers = {
+    '3 BANKA a.d. Novi Sad': '3BAN',
+    'ALTA BANKA A.D. BEOGRAD': 'ALT',
+    'API Bank akcionarsko društvo Beograd': 'API',
+    'Addiko Bank AD Beograd': 'ADIK',
+    'Adriatic Bank akcionarsko društvo Beograd': 'ADR',
+    'Agroindustrijsko komercijalna banka AIK banka akcionarsko društvo, Beograd': 'AIK',
+    'Banca Intesa A.D.- Beograd': 'BIT',
+    'Bank of China Srbija akcionarsko društvo Beograd - Novi Beograd': 'BOCS',
+    'Banka Poštanska štedionica A.D.- Beograd': 'POS',
+    'Erste Bank A.D.- Novi Sad': 'ERST',
+    'Eurobank Direktna akcionarsko društvo Beograd': 'EURO',
+    'Halkbank Akcionarsko društvo Beograd': 'HALK',
+    'MIRABANK AKCIONARSKO DRUSTVO BEOGRAD': 'MIRA',
+    'Mobi banka A.D. - Beograd': 'MOBI',
+    'NLB Komercijalna banka AD Beograd': 'NLB',
+    'OTP Banka Srbija a.d. Novi Sad': 'OTP',
+    'ProCredit Bank A.D.- Beograd': 'PRO',
+    'Raiffeisen Banka A.D.- Beograd': 'RAIF',
+    'Srpska banka A.D.- Beograd': 'SRPS',
+    'Unicredit Bank Srbija A.D.- Beograd': 'UNI',
+}
+
+# this url contains a table of bank names, along with links to their corresponding excel files
+# NOTE: we need to check if this is a permanent url or if they change it every year/quarter
+table_url = 'https://nbs.rs/static/nbs_site/gen/cirilica/50/vl_strukt/50b5.htm'
+
+#privremeno hard-coded
+sheets_folder = 'sheets/'
+
+# links_dictionary = get_excel_urls(table_url)
+    
+#test download_file function
+# for name,link in links_dictionary.items():
+#     download_file(link, sheets_folder)
+
+bank_data = parse_sheet_excel(sheets_folder)
+
+agg_frame = Agg_frame('test_sheet.xlsx')
+agg_frame.aggregate_bilans(bank_data)
+agg_frame.add_indicators()
+agg_frame.show_correlations()
+agg_frame.hierarchical_clustering()
+agg_frame.kmeans()
+agg_frame.perform_pca_and_cluster()
+# agg_frame.print_dataframe()
+# agg_frame.output_file('test_sheet.xlsx')
+
+UKUPNO_AKTIVA_2023 = agg_frame.dataframe[agg_frame.dataframe['Godina'] == 2023]['UKUPNO AKTIVA'].sum()
+UKUPNO_AKTIVA_2022 = agg_frame.dataframe[agg_frame.dataframe['Godina'] == 2022]['UKUPNO AKTIVA'].sum()
+RAST_AKTIVE = round((UKUPNO_AKTIVA_2023 / UKUPNO_AKTIVA_2022-1)*100,2)
+UKUPAN_DEPOZIT_2023 = agg_frame.dataframe[agg_frame.dataframe['Godina'] == 2023]['Depoziti i ostale finansijske obaveze prema drugim komitentima'].sum()
+UKUPAN_DEPOZIT_2022 = agg_frame.dataframe[agg_frame.dataframe['Godina'] == 2022]['Depoziti i ostale finansijske obaveze prema drugim komitentima'].sum()
+RAST_DEPOZITA = round((UKUPAN_DEPOZIT_2023 / UKUPAN_DEPOZIT_2022-1)*100,2)
+UKUPAN_KREDIT_2023 = agg_frame.dataframe[agg_frame.dataframe['Godina'] == 2023]['Krediti i potraživanja od komitenata'].sum()
+UKUPAN_KREDIT_2022 = agg_frame.dataframe[agg_frame.dataframe['Godina'] == 2022]['Krediti i potraživanja od komitenata'].sum()
+RAST_KREDITA = round((UKUPAN_KREDIT_2023 / UKUPAN_KREDIT_2022-1)*100,2)
 
 # Sample data
 data = pd.DataFrame({
@@ -15,12 +77,9 @@ data = pd.DataFrame({
     'MarketValue': [1000, 1500, 2000,3000, 500, 1000, 1500, 2000],
 })
 
-data_treemap = pd.DataFrame({
-    'labels': ['A', 'B', 'C', 'D','E','F','G','H','I','J'],
-    #random array of percentages (adding up to 100)
-    'values': list(np.random.dirichlet(np.ones(10),size=1).transpose()*100)
-})
-
+# Filter godina 2023 and show only column 'Udeo na tržištu'
+data_treemap = agg_frame.dataframe[agg_frame.dataframe['Godina']==2023][['Banka','Udeo na tržištu']].reset_index(drop=True)
+data_treemap['Ticker'] = data_treemap['Banka'].map(tickers)
 # Function to create Seaborn chart
 def create_grouped_barplot(data, banks, marketvalue, year, title):
     fig, ax = plt.subplots()
@@ -39,13 +98,13 @@ def create_treemap(data):
     fig, ax = plt.subplots()
     colors = sns.color_palette("husl", n_colors=len(data))
     sns.set_style(style="whitegrid") # set seaborn plot style
-    sizes= data["values"].values# proportions of the categories
-    label_percent=data["labels"]+' '+data["values"].astype(str) + '%' # labels for categories
+    sizes= data["Udeo na tržištu"].values# proportions of the categories
+    label_percent=data["Ticker"]+' '+round(data["Udeo na tržištu"],2).astype(str) + '%' # labels for categories
     squarify.plot(sizes=sizes, label=label_percent, alpha=0.6,color=colors,ax=ax)
     # Ensure each element of the plot is labeled
     for i, rect in enumerate(ax.patches):
-        rect.name = data["labels"][i]
-    ax.set_title('Treemap with Squarify')
+        rect.name = data["Banka"][i]
+    ax.set_title('Udeo na tržištu u 2023. godini')
     plt.axis('off')
     return fig
 
@@ -93,7 +152,7 @@ container.grid_columnconfigure(0, weight=1)
 # Create frames for each page
 entire_market_frame = ctk.CTkFrame(container)
 selected_bank_frames = {}
-for label in data_treemap['labels']:
+for label in data_treemap['Banka']:
     selected_bank_frames[label] = ctk.CTkFrame(container)
 
 entire_market_frame.grid(row=0, column=0, sticky="nsew")
@@ -141,16 +200,19 @@ hover_cursor.annotation_kwargs = {'alpha':0}
 # numerical_data_label = ctk.CTkLabel(data_frame, text="Numerical Data", font=("Arial", 18))
 # numerical_data_label.grid(pady=10)
 
+BOX_LABELS = ['UKUPNO AKTIVA 2023', 'RAST AKTIVE', 'UKUPAN DEPOZIT 2023', 'RAST DEPOZITA', 'UKUPAN KREDIT 2023', 'RAST KREDITA']
+BOX_DATA = [UKUPNO_AKTIVA_2023, RAST_AKTIVE, UKUPAN_DEPOZIT_2023, RAST_DEPOZITA, UKUPAN_KREDIT_2023, RAST_KREDITA]
+
 # Add 6 boxes with labels and data to 'data' frame
 for i in range(3):
     for j in range(2):
         box_frame = ctk.CTkFrame(data_frame)
         box_frame.grid(row=i, column=j, padx=10, pady=10, sticky="nsew")
         
-        label = ctk.CTkLabel(box_frame, text=f"Label {i*2 + j + 1}", font=("Arial", 14))
+        label = ctk.CTkLabel(box_frame, text=BOX_LABELS[i*2 + j], font=("Arial", 24))
         label.pack(pady=5)
         
-        data_content = ctk.CTkLabel(box_frame, text=f"Data {i*2 + j + 1}", font=("Arial", 12))
+        data_content = ctk.CTkLabel(box_frame, text= str(BOX_DATA[i*2 + j]) + '%', font=("Arial", 30))
         data_content.pack(pady=5)
 
 # Configure the data_frame to expand and fill the available space
@@ -199,10 +261,10 @@ for label,bank_frame in selected_bank_frames.items():
 
     # NOTE: comment this out when debugging for better performance
     # Add content to 'chart' frame
-    bank_market_charts[label] = create_grouped_barplot(data, "Banks","MarketValue","Year", f"{label} Bank Market Value")
-    canvas = FigureCanvasTkAgg(bank_market_charts[label], master=bank_chart_frames[label])
-    canvas.draw()
-    canvas.get_tk_widget().pack(side="top", fill="both", expand=True)
+    # bank_market_charts[label] = create_grouped_barplot(data, "Banks","MarketValue","Year", f"{label} Bank Market Value")
+    # canvas = FigureCanvasTkAgg(bank_market_charts[label], master=bank_chart_frames[label])
+    # canvas.draw()
+    # canvas.get_tk_widget().pack(side="top", fill="both", expand=True)
 
     # Add 6 boxes with labels and data to 'data' frame
     for i in range(3):
